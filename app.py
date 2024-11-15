96b2a71b28f82f48aa597e971e1c41c82e799dae
Besto-Apis <bestasara123@gmail.com>
2024-11-15 20:06:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,46 +2,66 @@ import requests
 import re
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
-from flask import Flask, request, jsonify
+from flask import Flask, request, Response
 from datetime import datetime
 import json
+from concurrent.futures import ThreadPoolExecutor
 
 app = Flask(__name__)
 
-def enc(id):
-    url = "https://besto-api-enc.vercel.app/Enc/12345678?Key=Besto-K7J9"
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            match = re.search(r"EncryPted Id : (\S+)", response.text)
-            if match:
-                Enc_Iddd = match.group(1)
-                return Enc_Iddd
-        return " - Besto Off The Server !"
-    except Exception as e:
-        return f"Error in enc function: {str(e)}"
-
-def decrypt_api(cipher_text):
-    key = bytes([89, 103, 38, 116, 99, 37, 68, 69, 117, 104, 54, 37, 90, 99, 94, 56])
-    iv = bytes([54, 111, 121, 90, 68, 114, 50, 50, 69, 51, 121, 99, 104, 106, 77, 37])
-    try:
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-        plain_text = unpad(cipher.decrypt(bytes.fromhex(cipher_text)), AES.block_size)
-        return plain_text.hex()
-    except Exception as e:
-        return f"Error in decrypt_api function: {str(e)}"
+def enc(x):
+    x = int(x)
+    dec = ['80', '81', '82', '83', '84', '85', '86', '87', '88', '89', '8a', '8b', '8c', '8d', '8e', '8f', 
+           '90', '91', '92', '93', '94', '95', '96', '97', '98', '99', '9a', '9b', '9c', '9d', '9e', '9f',
+           'a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'aa', 'ab', 'ac', 'ad', 'ae', 'af',
+           'b0', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'ba', 'bb', 'bc', 'bd', 'be', 'bf',
+           'c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'ca', 'cb', 'cc', 'cd', 'ce', 'cf',
+           'd0', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9', 'da', 'db', 'dc', 'dd', 'de', 'df',
+           'e0', 'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'e9', 'ea', 'eb', 'ec', 'ed', 'ee', 'ef',
+           'f0', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'fa', 'fb', 'fc', 'fd', 'fe', 'ff']
+    xxx = ['1', '01', '02', '03', '04', '05', '06', '07', '08', '09', '0a', '0b', '0c', '0d', '0e', '0f',
+           '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '1a', '1b', '1c', '1d', '1e', '1f',
+           '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '2a', '2b', '2c', '2d', '2e', '2f',
+           '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '3a', '3b', '3c', '3d', '3e', '3f',
+           '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '4a', '4b', '4c', '4d', '4e', '4f',
+           '50', '51', '52', '53', '54', '55', '56', '57', '58', '59', '5a', '5b', '5c', '5d', '5e', '5f',
+           '60', '61', '62', '63', '64', '65', '66', '67', '68', '69', '6a', '6b', '6c', '6d', '6e', '6f',
+           '70', '71', '72', '73', '74', '75', '76', '77', '78', '79', '7a', '7b', '7c', '7d', '7e', '7f']
+    
+    x = x / 128
+    if x > 128:
+        x = x / 128
+        if x > 128:
+            x = x / 128
+            if x > 128:
+                x = x / 128
+                strx = int(x)
+                y = (x - int(strx)) * 128
+                stry = str(int(y))
+                z = (y - int(stry)) * 128
+                strz = str(int(z))
+                n = (z - int(strz)) * 128
+                strn = str(int(n))
+                m = (n - int(strn)) * 128
+                return dec[int(m)] + dec[int(n)] + dec[int(z)] + dec[int(y)] + xxx[int(x)]
+            else:
+                strx = int(x)
+                y = (x - int(strx)) * 128
+                stry = str(int(y))
+                z = (y - int(stry)) * 128
+                strz = str(int(z))
+                n = (z - int(strz)) * 128
+                strn = str(int(n))
+                return dec[int(n)] + dec[int(z)] + dec[int(y)] + xxx[int(x)]
 
 def encrypt_api(plain_text):
     plain_text = bytes.fromhex(plain_text)
     key = bytes([89, 103, 38, 116, 99, 37, 68, 69, 117, 104, 54, 37, 90, 99, 94, 56])
     iv = bytes([54, 111, 121, 90, 68, 114, 50, 50, 69, 51, 121, 99, 104, 106, 77, 37])
-    try:
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-        cipher_text = cipher.encrypt(pad(plain_text, AES.block_size))
-        return cipher_text.hex()
-    except Exception as e:
-        return f"Error in encrypt_api function: {str(e)}"
-        
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    cipher_text = cipher.encrypt(pad(plain_text, AES.block_size))
+    return cipher_text.hex()
+
 def Token():
     api_url = "https://besto-get-access-token-uid.vercel.app/Token?Uid=3485092977&Pw=56AB38A686BD86A4C679B620C15C0B3BEA50D04A4F38D13932BC82D4D228AB16"
     response_text = requests.get(api_url).text
@@ -55,11 +75,11 @@ def Token():
         if a3:
             return a3.group(1)
         else:
-            print("لم يتم العثور على Token في الاستجابة.")
+            return "لم يتم العثور على Token في الاستجابة."
     else:
-        print("لم يتم العثور على Access Token أو Access Id.")
-                
-def Add_Fr(id,Tok):
+        return "لم يتم العثور على Access Token أو Access Id."
+
+def Add_Fr(id, Tok):
     url = 'https://clientbp.common.ggbluefox.com/RequestAddingFriend'
     headers = {
         'X-Unity-Version': '2018.4.11f1',
@@ -90,13 +110,16 @@ def Add_Fr(id,Tok):
 @app.route('/Add', methods=['GET'])
 def add_friend():
     id = request.args.get('Id')
-    
+    Besto = []
     if id:
-        Tok = Token()
-        response = Add_Fr(id,Tok)
-        return jsonify({" - ResPonse > ": response})
+        with ThreadPoolExecutor(max_workers=100) as executor:
+            Tok = executor.submit(Token)
+            Tok = Tok.result()      
+            response = Add_Fr(id,Tok)
+        Besto.append(f' - ResPonse : {response}')
     else:
-        return jsonify({"error": "ID is required"}), 400
+        Besto.append(' - No Id ! '), 400
+    return Response("\n".join(Besto), mimetype='text/plain')
 
 if __name__ == '__main__':
     app.run()
